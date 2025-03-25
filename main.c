828397a201c458dae268c0008da5cdf06f95de02
Alejandro Antelo <alfasant@protonmail.com>
2025-03-25 10:57:14 +0100

    monospace

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include "raylib/include/raylib.h"
 #include <stdio.h>
-#include <ctype.h>
 
 #define UNPACK_RECT(rect) (rect).x, (rect).y, (rect).width, (rect).height
 #define UNPACK_RECT_PAD(rect, n)                 \
     (rect).x - (n), (rect).y - (n),              \
     (rect).width + (n)*2, (rect).height + (n)*2
-#define POSITION_TO_VECTOR2(pos) CLITERAL(Vector2) {(pos).x, (pos).y}
-#define VECTOR2_TO_POSITION(vec) CLITERAL(position) {(int)(vec).x, (int)(vec).y}
+#define VECTOR2_RECT(rect) CLITERAL(Vector2) { (rect.x), (rect.y) }
+#define VECTOR2_RECT_PAD(rect, n) CLITERAL(Vector2) { (rect.x) + (n), (rect.y) + (n) }
 
 // TODO: Looks like text must be monospaced for the effect to work correctly
 
@@ -15,9 +14,9 @@ typedef struct {
     int x, y;
 } Position;
 
-int MeasureChar(char c, int fontSize) {
+int MeasureChar(Font font, char c, int fontSize, int spacing) {
     char buf[2] = " "; buf[0] = c;
-    return MeasureText(buf, fontSize);
+    return MeasureTextEx(font, buf, fontSize, spacing).x;
 }
 
 int main()
@@ -28,8 +27,11 @@ int main()
     SetTargetFPS(60);
 
     const int fontSize = 20; // if using the default font, should be a multiple of 10
+    Font font = LoadFontEx("resources/UbuntuMono-R.ttf", fontSize, 0, 250);
     // spacing = fontSize/defaultFontSize;
-    const int spacing = fontSize/GetFontDefault().baseSize;
+    const int spacing = 0;
+    const int charWidth = MeasureChar(font, 'M', fontSize, spacing);
+
     char text[512];
     for (int i = 0; i < sizeof(text)-2; i++) {
         text[i] = ' ';
@@ -47,7 +49,7 @@ int main()
 
     Position cursorPos = (Position) {0, 0};
     Rectangle cursor = (Rectangle) {
-        .width = MeasureChar('M', fontSize),
+        .width = charWidth,
         .height = fontSize,
         .x = container.x + padding,
         .y = container.y + padding
@@ -58,11 +60,9 @@ int main()
     while (!WindowShouldClose())
     {
         int key;
-        int charWidth;
         while ((key = GetCharPressed()) > 0) {
-            charWidth = MeasureChar(toupper(key), fontSize);
             if ((key >= 33) && (key <= 125) && (lineWidth + charWidth + spacing < container.width-padding*2)) {
-                text[cursorPos.x++] = toupper(key);
+                text[cursorPos.x++] = key;
                 textLength = cursorPos.x > textLength ? cursorPos.x : textLength;
                 lineWidth += charWidth + spacing;
                 printf("%d\n", key);
@@ -70,12 +70,13 @@ int main()
         }
 
         if (IsKeyPressed(KEY_SPACE) && (lineWidth + cursor.width + spacing < container.width-padding*2)) {
-            lineWidth += MeasureChar(text[cursorPos.x++], fontSize) + spacing;
+            lineWidth += charWidth + spacing;
+            cursorPos.x++;
             textLength = cursorPos.x > textLength ? cursorPos.x : textLength;
         }
         if (IsKeyPressed(KEY_BACKSPACE) && cursorPos.x > 0) {
             cursorPos.x--;
-            lineWidth -= MeasureChar(text[cursorPos.x], fontSize) + spacing;
+            lineWidth -= charWidth + spacing;
         }
         cursor.x = container.x + padding + lineWidth;
 
@@ -85,12 +86,13 @@ int main()
             DrawRectangle(UNPACK_RECT_PAD(container, border), BLACK);
             DrawRectangleRec(container, WHITE);
             DrawRectangleRec(cursor, LIGHTGRAY);
-            DrawText(text, container.x+padding, container.y+padding, fontSize, BLACK);       // Draw text (using default font)
+            DrawTextEx(font, text, VECTOR2_RECT_PAD(container, padding), font.baseSize, spacing, BLACK);
             /* DrawTextBoxed(GetFontDefault(), text, (Rectangle) { UNPACK_RECT_PAD(container, -padding) }, fontSize, spacing, true, BLACK); */
         }
         EndDrawing();
     }
 
+    UnloadFont(font);
     CloseWindow();
     return 0;
 }
