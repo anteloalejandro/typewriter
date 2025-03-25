06e3e5f88edaa72a694347281c3b400acb47b90b
Alejandro Antelo <alfasant@protonmail.com>
2025-03-25 06:36:33 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 LIBS="-lGL -lm -lpthread -ldl -lrt -lX11 -Lraylib/include/lib -lraylib"
-OPTIONS=""
+OPTIONS="$@"
 DEBUG=1
 
 if test $DEBUG -ne 0; then OPTIONS="${OPTIONS} -ggdb"; fi
 
+mkdir -p bin
 gcc main.c -o bin/main $LIBS $OPTIONS
