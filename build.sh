84945ceaf825b27fdd3266a69d468238c19e7c7d
Alejandro Antelo <alfasant@protonmail.com>
2025-03-29 02:43:33 +0100

    fix path to raylib

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-LIBS="-lGL -lm -lpthread -ldl -lrt -lX11 -Lraylib/include/lib -lraylib"
+LIBS="-lGL -lm -lpthread -ldl -lrt -lX11 -Lraylib/lib -lraylib"
 FILES=$(ls src/*)
 OPTIONS="-Wall -Werror -Wextra $@"
 DEBUG=1
